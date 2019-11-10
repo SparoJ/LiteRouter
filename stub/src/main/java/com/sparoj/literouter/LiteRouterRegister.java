9b1971b40d490689b39091ef5443c21be397d7a8
SparoJ <sdhchn@gmail.com>
2019-11-10 23:35:31 +0800

    add stub for connection

---
+++ stub/src/main/java/com/sparoj/literouter/LiteRouterRegister.java
@@ -0,0 +1,13 @@
+package com.sparoj.literouter;
+
+import java.util.HashMap;
+
+/**
+ * 占位使用
+ */
+public class LiteRouterRegister {
+
+    public static void register(HashMap moduleMap, HashMap actionMap) {
+
+    }
+}
